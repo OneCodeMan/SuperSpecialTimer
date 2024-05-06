ba68675f63ae06445a7a76ec902bfa57d5f25af1
Dave Gumba <davegumba@gmail.com>
2024-05-06 19:02:33 -0400

    Initial Commit

---
+++ SuperSpecialTimer/SuperSpecialTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSpecialTimerApp.swift
+//  SuperSpecialTimer
+//
+//  Created by Dave Gumba on 2024-05-06.
+//
+
+import SwiftUI
+
+@main
+struct SuperSpecialTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
